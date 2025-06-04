aba7eb478adcbd3a3bb8b97d8ae5d85a8f67814c
Spencer Rafada <spencer.byui@gmail.com>
2025-06-04 10:17:40 -0600

    feat: setup the client and importer for v1 iteration

--- app.rb
+++ app.rb
@@ -12,4 +12,38 @@ puts "Found #{repos.count} repos for #{org}"
 
 repos.each do |repo|
   puts "#{repo.full_name} - #{repo.html_url}"
+
+  # puts "Fetching pull requests for #{repo.full_name}..."
+  # pull_requests = importer.fetch_pull_requests(org, repo.full_name)
+
+  # puts "Found #{pull_requests.count} pull requests for #{repo.full_name}"
+end
+
+puts "Fetching pull request for Next.js repo..."
+pull_requests = importer.fetch_pull_requests('vercel/next.js')
+
+puts "Found #{pull_requests.count} pull requests for Next.js"
+
+pull_requests.each do |pull_request|
+  puts "#{pull_request.number}: #{pull_request.title} - #{pull_request.html_url}"
+end
+
+pr_details = importer.fetch_pull_request_details('vercel/next.js', 80160)
+
+puts "PR ##{pr_details.number}: #{pr_details.title}"
+puts "Author: #{pr_details.user.login}"
+puts "Updated: #{pr_details.updated_at}"
+puts "Closed: #{pr_details.closed_at}"
+puts "Merged: #{pr_details.merged_at}"
+puts "Additions: #{pr_details.additions}"
+puts "Deletions: #{pr_details.deletions}"
+puts "Changed Files: #{pr_details.changed_files}"
+# puts "Commits: #{pr_details.commits}" # Commits
+
+puts "Fetching reviews for PR ##{pr_details.number}..."
+reviews = importer.fetch_pull_request_reviews('vercel/next.js', pr_details.number)
+
+puts "Found #{reviews.count} reviews for PR ##{pr_details.number}"
+reviews.each do |review|
+  puts "#{review.user.login} - #{review.state} at #{review.submitted_at}"
 end
--- lib/github/importer.rb
+++ lib/github/importer.rb
@@ -9,5 +9,17 @@ module GitHub
     def fetch_repos(org, limit: 5)
       @client.org_repos(org, per_page: limit)
     end
+
+    def fetch_pull_requests(repo, limit: 5)
+      @client.pull_requests(repo, per_page: limit)
+    end
+
+    def fetch_pull_request_details(repo, pull_request_number)
+      @client.pull_request(repo, pull_request_number)
+    end
+
+    def fetch_pull_request_reviews(repo, pull_request_number)
+      @client.pull_request_reviews(repo, pull_request_number)
+    end
   end
 end
